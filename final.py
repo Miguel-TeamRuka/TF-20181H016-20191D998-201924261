0bc783d9511bde9aa70d314570574f4e487b95db
Miguel Benavides <u20191d998@upc.edu.pe>
2022-06-25 22:48:27 -0500

    Pasar a funciones 2 #6
    Se paso a funcion la generacion del perlin noise ,la actualizacion por hora

--- final.py
+++ final.py
@@ -112,3 +112,175 @@ def distancia(origen,destino,nodes):
   nodeD=nodes[destino]
   d=haversine(nodeO[3],nodeO[4],nodeD[3],nodeD[4])
   return d
+
+def calcularPeso(nroAutos,velocidad,distancia):
+  return velocidad*(nroAutos/distancia)
+
+def GenerarPerlinNoise():
+    noise1 = PerlinNoise(octaves=2)
+    noise2 = PerlinNoise(octaves=4)
+    noise3 = PerlinNoise(octaves=8)
+    noise4 = PerlinNoise(octaves=12)
+
+    xpix, ypix = 51,41
+    pic = []
+    for i in range(xpix):
+        row = []
+        for j in range(ypix):
+            noise_val = noise1([i/xpix, j/ypix])
+            noise_val += 0.100 * noise2([i/xpix, j/ypix])
+            noise_val += 0.070 * noise3([i/xpix, j/ypix])
+            noise_val += 0.035 * noise4([i/xpix, j/ypix])
+            row.append(noise_val)
+      
+        pic.append(row)
+
+    #plt.imshow(pic, cmap='gray')
+    #plt.show()
+    return pic
+
+def reglas(m,n,pic):
+  if pic[m][n]>=-1 and pic[m][n]<-0.5:
+    return random.randint(47, 52),random.randint(10,20)
+    #return 30,12
+  if pic[m][n]>=-0.5 and pic[m][n]<-0.2:
+    return random.randint(41,46),random.randint(21,28)
+    #return 26,20
+  if pic[m][n]>=-0.2 and pic[m][n]<0.2:
+    return random.randint(31,40),random.randint(29, 34)
+    #return 20,32
+  if pic[m][n]>=0.2 and pic[m][n]<0.5:
+    return random.randint(21,30),random.randint(34,39)
+    #return 16,36
+  if pic[m][n]>=0.5 and pic[m][n]<=1:
+    return random.randint(10, 20),random.randint(40,45)
+    #return 10,40
+#print(pic[5][26])
+#print(reglas(5,26))
+#print(pic[39][26])
+#print(reglas(39,26))
+
+def returnCoefHour():
+  now = datetime.now()
+  
+  if now.hour>=0 and now.hour<7:
+    return float(decimal.Decimal(random.randrange(60, 80))/100)
+    #return 0.5
+  if now.hour>=7 and now.hour<10:
+    return float(decimal.Decimal(random.randrange(120, 140))/100)
+    #return 1.2
+  if now.hour >=10 and now.hour<18: 
+    return float(decimal.Decimal(random.randrange(100, 110))/100)
+    #return 1
+  if now.hour >=18 and now.hour<22:   
+    return float(decimal.Decimal(random.randrange(140, 160))/100)
+    #return 1.25
+  if now.hour >=22 and now.hour<0:
+    return float(decimal.Decimal(random.randrange(80, 95))/100)
+    #return 0.9
+  if now.hour==22:
+    return float(decimal.Decimal(random.randrange(80, 95))/100)
+    #return 0.9
+  return 1
+
+def pesoFinal(origin,neighbort,picI,picJ,nodes,perlin):
+  distanciaValor = distancia(origin,neighbort,nodes)
+  nroAutos,velocidad =reglas(picI,picJ,perlin)
+  return round(calcularPeso(nroAutos,velocidad,distanciaValor)*returnCoefHour(),2)
+
+
+def GenerarAristas(hor,ver,b,nodes,perlin):
+  listAd=[]
+  auxiliar=[]
+  for i in range(len(hor)):
+    for j in range(len(ver)):
+      if b[i][j]!=-1:
+        if j<len(ver)-1: #final de una fila
+          if b[i][j+1]!=-1:
+            right=b[i][j+1]
+            origin=b[i][j]
+            #nodeO=nodes[origin]
+            #nodeD=nodes[right]
+            #d=haversine(nodeO[3],nodeO[4],nodeD[3],nodeD[4])
+            #peso=distancia(origin,right)
+            peso= pesoFinal(origin,right,i,j+1,nodes,perlin)
+            auxiliar.append((right,peso))
+      if j>0: #inicio de una fila
+          if b[i][j-1]!=-1:
+            left=b[i][j-1]
+            origin=b[i][j]
+            #nodeO=nodes[origin]
+            #nodeD=nodes[left]
+            #d=haversine(nodeO[3],nodeO[4],nodeD[3],nodeD[4])
+            #peso=distancia(origin,left)
+            peso= pesoFinal(origin,left,i,j-1,nodes,perlin)
+            auxiliar.append((left,peso))
+      if i<len(hor)-1: #final de una columna
+          if b[i+1][j]!=-1:
+            bottom=b[i+1][j]
+            origin=b[i][j]
+            #nodeO=nodes[origin]
+            #nodeD=nodes[bottom]
+            #d=haversine(nodeO[3],nodeO[4],nodeD[3],nodeD[4])
+            #peso=distancia(origin,bottom)
+            peso= pesoFinal(origin,bottom,i+1,j,nodes,perlin)
+            auxiliar.append((bottom,peso))
+      if i>0: #inicio de una columna
+          if b[i-1][j]!=-1:
+            top=b[i-1][j]
+            origin=b[i][j]
+            #nodeO=nodes[origin]
+            #nodeD=nodes[top]
+            #d=haversine(nodeO[3],nodeO[4],nodeD[3],nodeD[4])
+            #peso=distancia(origin,top)
+            peso= pesoFinal(origin,top,i-1,j,nodes,perlin)
+            auxiliar.append((top,peso))
+      listAd.append(auxiliar)
+      auxiliar=[]
+
+  return listAd
+  #print(listAd)
+  #for l in listAd:
+  #  print(l)
+
+def EscribirListaAd(listAd):
+    c=0
+    with open('ListaAdyacencia.txt', 'w') as f:
+        for i in range(len(listAd)):
+            for j in listAd[i]:
+                if(c==len(listAd[i])-1):
+                    #f.write(str(j))
+                    f.write(re.sub(r'[\(\,)]','',str(j)))
+                else:
+                    #f.write(str(j)+" ")
+                    f.write(re.sub(r'[\(\,)]','',str(j)+" "))
+                c=c+1
+            c=0
+            if i<len(listAd)-1:
+                f.write('\n')
+def LeerListaAd():
+    generarListaAd()
+    with open("ListaAdyacencia.txt") as f:
+        Gr = []
+        for line in f:
+            nums=[]
+    
+            for count,x in enumerate(line.split()):
+                if count%2==0:
+                    nums.append(int(x))
+                else:
+                    nums.append(float(x))
+            Gr.append([])
+
+            for i in range(0, len(nums), 2):
+                Gr[-1].append((nums[i], nums[i+1]))
+
+   # nums = [float(x) for x in line.split()]
+   # G.append([])
+   # for i in range(0, len(nums), 2):
+   #   G[-1].append((nums[i], nums[i+1]))
+   # for x in Gr:
+   #     print(x)
+    return Gr
+    #for x in Gr:
+    #    print(x)
